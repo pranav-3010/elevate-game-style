cd1d7bb8a6afbe80298070be206f6c61075a9d51
BALA PRANAV <pranav@PRANAVS-MACBOOK-AIR.local>
2026-07-01 18:57:52 +0530

    fix: Correct profiles RLS policy comparison types

--- src/lib/schema.sql
+++ src/lib/schema.sql
@@ -46,10 +46,10 @@ create policy "Allow public read-only profiles" on public.profiles
   for select using (true);
 
 create policy "Allow users to update their own profile" on public.profiles
-  for update using (auth.uid() = id OR (select current_setting('request.jwt.claims', true)::json->>'sub') = id);
+  for update using ((select current_setting('request.jwt.claims', true)::json->>'sub') = id);
 
 create policy "Allow users to insert their own profile" on public.profiles
-  for insert with check (auth.uid() = id OR (select current_setting('request.jwt.claims', true)::json->>'sub') = id);
+  for insert with check ((select current_setting('request.jwt.claims', true)::json->>'sub') = id);
 
 -- Orders Policies
 create policy "Allow users to view their own orders" on public.orders
